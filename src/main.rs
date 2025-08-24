95d105a03ff6b53000d5d4f4c47019da78d0c1cd
Sathya <neelasathya06@gmail.com>
2025-08-24 14:23:58 -0400

    first and last name with slices impl

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,33 @@ fn do_hero_stuff(hero_name: &str) {
 
 fn main() {
 
-    let action_hero = String::from("Sathya Ram");
-    
-    do_hero_stuff(&action_hero);
-    
-    let another_hero = "Vinothini";
-    do_hero_stuff(&another_hero);
-    // 
+    // let action_hero = String::from("Sathya Ram");
+    //
+    // do_hero_stuff(&action_hero);
+    //
+    // let another_hero = "Vinothini";
+    // do_hero_stuff(&another_hero);
+    //
     // let first_name = &action_hero[..6];
-    // 
+    //
     // let last_name = &action_hero[7..];
-    // 
+    //
     // println!("{first_name}");
-    // 
+    //
     // println!("{last_name}");
-    // 
-    // 
+    //
+    //
     // let food = "pizza";
     // println!("{}", food.len());
-    // 
+    //
     // let pizza_slice = &food[0..3];
     // println!("{}", pizza_slice.len());
+
+//     Array Slices
+    let values = [4, 8, 15,16,23, 42];
+
+    let my_slice = &values[1..];
+    println!("{my_slice:?}");
+
 
 }
