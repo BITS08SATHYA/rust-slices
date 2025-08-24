436812171ccd12103529f68d6583a85c709cfaa5
Sathya <neelasathya06@gmail.com>
2025-08-24 14:19:32 -0400

    first and last name with slices impl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
+use std::os::unix::fs::lchown;
+
+fn do_hero_stuff(hero_name: &str) {
+    println!("{hero_name} saves the day!");
+}
+
 fn main() {
 
     let action_hero = String::from("Sathya Ram");
-
-    let first_name = &action_hero[..6];
     
-    let last_name = &action_hero[7..];
-
-    println!("{first_name}");
+    do_hero_stuff(&action_hero);
     
-    println!("{last_name}");
-
-    let food = "pizza";
-    println!("{}", food.len());
-
-    let pizza_slice = &food[0..3];
-    println!("{}", pizza_slice.len());
+    let another_hero = "Vinothini";
+    do_hero_stuff(&another_hero);
+    // 
+    // let first_name = &action_hero[..6];
+    // 
+    // let last_name = &action_hero[7..];
+    // 
+    // println!("{first_name}");
+    // 
+    // println!("{last_name}");
+    // 
+    // 
+    // let food = "pizza";
+    // println!("{}", food.len());
+    // 
+    // let pizza_slice = &food[0..3];
+    // println!("{}", pizza_slice.len());
 
 }
