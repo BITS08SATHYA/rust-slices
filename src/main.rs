084c77c41ab3f4b7d76c555d32c5714fd168b2d8
Sathya <neelasathya06@gmail.com>
2025-08-24 14:07:03 -0400

    first and last name with slices impl

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ fn main() {
 
     let action_hero = String::from("Sathya Ram");
 
-    let first_name = &action_hero;
+    let first_name = &action_hero[..6];
+    
+    let last_name = &action_hero[7..];
 
     println!("{first_name}");
+    
+    println!("{last_name}");
 
     let food = "pizza";
     println!("{}", food.len());
